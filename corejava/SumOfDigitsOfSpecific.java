7e1fe0ccef722177343dcc79d67ab8a5f183c5eb
Aditi Kawale <aditikawale57@gmail.com>
2026-05-13 13:38:38 +0530

    add a sum of digits to add the individual number in the given number

---
+++ corejava/SumOfDigitsOfSpecific.java
@@ -0,0 +1,24 @@
+package corejava;
+
+import java.util.Scanner;
+
+public class SumOfDigitsOfSpecific {
+    public static void main(String[] args) {
+        System.out.print("Enter a number: ");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println("Sum of Digits is: "+sumOfDigit(n));
+        sc.close();
+    }
+
+    public static int sumOfDigit(int num){
+        int sum=0;
+        while (num!=0) {
+            int digit = num%10;
+            sum+=digit;
+            num=num/10;
+        }
+        return sum;
+    }
+
+}
