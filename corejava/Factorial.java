ed71de711b94c002e5e9b0fe5ba7094ea8422bc9
Aditi Kawale <aditikawale57@gmail.com>
2026-05-13 13:14:35 +0530

    add program to calculate factorial using recursion and without recursion

---
+++ corejava/Factorial.java
@@ -0,0 +1,34 @@
+package corejava;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number:");
+        int n=sc.nextInt();
+        System.out.println("Factorial of "+n+" is: "+ factorial(n));
+        sc.close();
+    }
+
+    //using recursion
+   /*  public static int factorial(int num){
+        if(num == 0 || num ==1)
+            return 1;
+        else{
+            return num*factorial(num-1);
+        }
+    }*/
+
+    //without recursion
+    public static int factorial(int num){
+        int fact=1;
+        int i=1;
+        while (i<=num) {
+           fact*=i; 
+           i++;
+        }
+        return fact;
+    }
+
+}
