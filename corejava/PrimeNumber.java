abe910f6e35dcf3edd36b9dce99be681127c1280
Aditi Kawale <aditikawale57@gmail.com>
2026-05-14 15:33:44 +0530

    add program for prime number

---
+++ corejava/PrimeNumber.java
@@ -0,0 +1,36 @@
+package corejava;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the no to check whether it is prime or not:");
+        int n=sc.nextInt();
+        if(isPrime(n))
+            System.out.println("Number is prime");
+        else
+            System.out.println("Number is not prime");
+        sc.close();
+    }
+    public static boolean isPrime(int num){
+        if(num <= 1)
+            return false;
+
+        if(num==2)
+            return true;
+
+        // Eliminate even numbers
+        if (num % 2 == 0)
+            return false;
+
+        // Check only odd divisors
+        for (int i = 3; i * i <= num; i += 2) {
+            if (num % i == 0)
+                return false;
+        }
+
+       return true;
+    }
+
+}
