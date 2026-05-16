6ce9dc0a78017b05ce2737cc40aa226d4110e1ed
Aditi Kawale <aditikawale57@gmail.com>
2026-05-16 13:46:43 +0530

    fibonacci implemeted using recursion

--- corejava/FibonacciSeries.java
+++ corejava/FibonacciSeries.java
@@ -7,19 +7,34 @@ public class FibonacciSeries {
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter the number:");
         int n=sc.nextInt();
-        fibonacci(n);
+        // fibonacci(n);
+        for(int i = 0; i < n; i++){
+    System.out.print(fibonacci(i) + " ");
+}
         sc.close();
     }
 
-    public static void fibonacci(int num){
-        if(num <= 0) return;
-        int first=0,second=1;
-        for(int i=1; i<= num; i++){
-            System.out.print(first+" ");
-            int next=first+second;
-            first=second;
-            second=next;
+    // public static void fibonacci(int num){
+    //     if(num <= 0) return;
+    //     int first=0,second=1;
+    //     for(int i=1; i<= num; i++){
+    //         System.out.print(first+" ");
+    //         int next=first+second;
+    //         first=second;
+    //         second=next;
+    //     }
+    // }
+
+    public static int fibonacci(int num){
+        
+        if(num ==0)
+            return 0;
+        if(num==1)
+            return 1;
+        else{
+            return fibonacci(num-1) + fibonacci(num - 2);
         }
+
     }
 
 
